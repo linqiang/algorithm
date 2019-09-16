06f07a077bd811c46aefcda1fe9567035eb6a8a3
Shen Zhi <woaigrj@gmail.com>
2019-09-17 01:39:48 +0800

    添加快速排序的简单OC实现方法

--- Algorithm-iOS/Algorithm-iOS/ViewController.m
+++ Algorithm-iOS/Algorithm-iOS/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view.
     NSMutableArray *muArr = [NSMutableArray arrayWithObjects:@(2),@(5),@(1),@(12),@(8), nil];
 //    [self buddleSort:muArr]; 冒泡排序
+    [self quickSort:muArr];
 }
 
 #pragma mark -- buddle Sort
@@ -50,11 +51,44 @@
 #pragma mark -- quickSort
 /**
  这里是快速排序，关于快速排序的简单思想是这样的
- 因为在OC中并没有所谓向量的概念，我们这边直接使用数组进行排序
- 在对一个数组进行排序之前，
- 我们需要找到一个相应的枢纽元，将数组分成两个部分，分别对枢纽元的左右两边进行排序，这里会用到划分算法进行递归处理
+ 我们这边直接使用数组进行排序，下面是对于快速排序的简单思路
+ 
+ 在对一个数组进行排序之前，我们可以通过分治算法来寻找到一个相应的枢纽元，将数组分成两个部分，分别对枢纽元的左右两边进行排序，这里会用到划分算法进行递归处理
  在C++ 和 C语言中都有相应的快速排序函数，但是又有些许区别，在C语言中，sort函数不仅仅是简单的快速排序大致是这样的(快速排序+ 堆排序+插入排序)
+ 
  如果有兴趣还可以去看《算法》这本书，毕竟作者是专业搞快速排序的
  */
 
+// 以下是快速排序的简单实现，当然这里并不是最正统的快速排序，具体需要再参看《算法》中的实现
+-(NSArray *)quickSort:(NSMutableArray *)unsortedDataArray
+{
+    NSUInteger quickSortCount = 0;
+
+    NSMutableArray *lessArray = [[NSMutableArray alloc] init];
+    NSMutableArray *greaterArray =[[NSMutableArray alloc] init];
+    if ([unsortedDataArray count] <1)
+    {
+        return nil;
+    }
+    int randomPivotPoint = arc4random() % [unsortedDataArray count];
+    NSNumber *pivotValue = [unsortedDataArray objectAtIndex:randomPivotPoint];
+    [unsortedDataArray removeObjectAtIndex:randomPivotPoint];
+    for (NSNumber *num in unsortedDataArray)
+    {
+        quickSortCount++; //查看使用快速排序对数组进行排序所需的迭代次数
+        if (num < pivotValue)
+        {
+            [lessArray addObject:num];
+        }
+        else
+        {
+            [greaterArray addObject:num];
+        }
+    }
+    NSMutableArray *sortedArray = [[NSMutableArray alloc] init];
+    [sortedArray addObjectsFromArray:[self quickSort:lessArray]];
+    [sortedArray addObject:pivotValue];
+    [sortedArray addObjectsFromArray:[self quickSort:greaterArray]];
+    return sortedArray;
+}
 @end
